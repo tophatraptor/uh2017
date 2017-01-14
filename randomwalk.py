a3856ddc208fd34aa47dcdfc519730df826b0a59
jmilbauer <jeremiahsafe@wireless-s0-so-150-35-74.uchicago.edu>
2017-01-14 16:03:43 -0600

    removed tupling

--- randomwalk.py
+++ randomwalk.py
@@ -1,8 +1,8 @@
 import random as r
 def getwalk(n):
-    arr = [(0,0)]
+    arr = [[0,0]]
     for i in range(0,n):
-        lastpos = [arr[i][0], arr[i][1]]
+        lastpos = arr[i]
         lastpos[r.randint(0,1)] += [-1,1][r.randint(0,1)]
-        arr += [(lastpos[0], lastpos[1])]
+        arr += [lastpos]
     return arr
